45cfc0335a50140d53f2e3efe14f6b17aad73be9
Nicci Sheets <niccisheets@aol.com>
2018-06-27 20:32:30 -0400

    step by step showing how reject method returns values that are not true only, failed for returned values

--- array_tdd_methods.rb
+++ array_tdd_methods.rb
@@ -1,4 +1,21 @@
-def array_reject_method()
-empty_array = []
-new_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+def beginning_array(original)
+	original
 end
+
+def array_reject_method(original)
+	new_array = []
+	original.each_with_index do |num, index|
+		if num[index] > 5
+			original = true
+		else num[index] < 5
+			original = false
+		end
+		original
+	end
+	new_array << original
+end
+
+def returned_values_rejected()
+end
+# def actual_reject_method(original)
+# end
--- test_array_tdd_methods.rb
+++ test_array_tdd_methods.rb
@@ -3,16 +3,26 @@ require_relative "array_tdd_methods.rb"
 
 class TestArrayTddMethods < Minitest::Test
 
-    def test_assert_returns_an_array
-        assert_equal(Array, array_reject_method().class)
-    end
+    # def test_assert_returns_an_array
+    #     assert_equal(Array, array_reject_method([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).class)
+    # end
 
     def test_assert_returns_elements_in_new_array
-    	assert_equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], array_reject_method())
+    	assert_equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], beginning_array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
     end
 
-    def test_assert_numbers_more_than_5_return
-    	assert_equal([6, 7, 8, 9, 10], array_reject_method())
+    def test_assert_rejects_numbers_less_than_5_as_false
+    	assert_equal([false], array_reject_method([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+    end
+
+    def test_assert_returns_values_rejeted_as_true
+    	assert_equal([1, 2, 3, 4], returned_values_rejected())
     end
     
+    # def test_assert_returns_only_elements_less_than_5_using_method
+    # 	assert_equal([1, 2, 3, 4], actual_reject_method([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+    # end
+
 end
+
+
