9c205989d32228c95797c3f8578a94cdd940a427
Nicci Sheets <niccisheets@aol.com>
2018-06-29 14:49:49 -0400

    failed test for same return without using the method of concat

--- string_tdd_methods.rb
+++ string_tdd_methods.rb
@@ -1,3 +1,7 @@
 def concat_method(first_string, second_string)
 	first_string.concat(second_string) #this is essentially just appending the second_string into the first_string, when calling upon this method 
 end
+
+
+def concat_without_method(first_string, second_string)
+end
--- test_string_tdd_methods.rb
+++ test_string_tdd_methods.rb
@@ -7,8 +7,10 @@ class TestStringTddMethods < Minitest::Test
 		assert_equal("Hello Nicci!", concat_method("Hello ", "Nicci!"))
 	end
 
+	def test_assert_that_returns_same_without_using_method
+	assert_equal("Hello Nicci!", concat_without_method("Hello ", "Nicci"))
+	end
 	
-
 
 end
 
