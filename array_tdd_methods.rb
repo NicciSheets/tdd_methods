a527283b9cf69987295d8bdd2614cbf6f0896057
Nicci Sheets <niccisheets@aol.com>
2018-06-28 23:14:24 -0400

    starting over, failed for using actual array method

--- array_tdd_methods.rb
+++ array_tdd_methods.rb
@@ -1,28 +1,60 @@
-def beginning_array(original)
-	original
+
+def reject_method(test_values)
 end
 
-def array_reject_method(original)
-	new_array = []
-	original.each_with_index do |num, index|
-		if num[index] > 5
-			original = true
-		else num[index] < 5
-			original = false
-		end
-		original
-	end
-	new_array << original
-end
 
-def returned_values_rejected(original)
-	empty = []
-	original.each_with_index do 
-		rejected = array_reject_method(original)
-		if original == false
-		p num = num
-		end
-		empty << num
-	end
-	empty
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def beginning_array(original)
+# 	original
+# end
+
+# def array_reject_method(original)
+# 	new_array = []
+# 	original.each_with_index do |num, index|
+# 		if num[index] > 5
+# 			original = true
+# 		else num[index] < 5
+# 			original = false
+# 		end
+# 		original
+# 	end
+# 	new_array << original
+# end
+# # 
+# def returned_values_rejected(original2)
+# 	empty = []
+# 	original2.each do 
+# 		rejected = array_reject_method(original2)
+# 		rejected.each_with_index do |original2, num, index|
+# 			if original2 == false
+# 				rejected = num[index]
+# 				empty << rejected
+# 			end
+# 		end
+# 	end
+# 	empty 
+# end
--- test_array_tdd_methods.rb
+++ test_array_tdd_methods.rb
@@ -3,26 +3,9 @@ require_relative "array_tdd_methods.rb"
 
 class TestArrayTddMethods < Minitest::Test
 
-    # def test_assert_returns_an_array
-    #     assert_equal(Array, array_reject_method([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).class)
-    # end
-
-    def test_assert_returns_elements_in_new_array
-    	assert_equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], beginning_array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+    def test_assert_that_using_reject_method_returns_false_values
+    	assert_equal([3, 4, 5, 6], reject_method([1, 2, 3, 4, 5, 6]))
     end
-
-    def test_assert_rejects_numbers_less_than_5_as_false
-    	assert_equal([false], array_reject_method([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-    end
-
-    def test_assert_returns_values_rejeted_as_true
-    	assert_equal([1, 2, 3, 4], returned_values_rejected([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-    end
-
-    # def test_assert_returns_only_elements_less_than_5_using_method
-    # 	assert_equal([1, 2, 3, 4], actual_reject_method([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-    # end
-
 end
 
 
